b9731305feb4b59a331a94e0867c97871614f56b
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-03 20:15:09 +0300

    Add project files.

---
+++ Vowel_count/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowel_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of strings: ");
+            int n = int.Parse(Console.ReadLine());
+            string[] cases = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter text: ");
+                cases[i] = Console.ReadLine();
+            }
+            
+        }
+    }
+}
