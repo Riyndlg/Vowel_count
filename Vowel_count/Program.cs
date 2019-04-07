724fafde0266922e6d5941daf8d6b5d60d45e2c8
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-07 11:13:57 +0300

    Working version, not ideal

--- Vowel_count/Program.cs
+++ Vowel_count/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Vowel_count
 {
@@ -18,7 +19,31 @@ namespace Vowel_count
                 Console.WriteLine("Enter text: ");
                 cases[i] = Console.ReadLine();
             }
-            
+            int count;
+            string test;
+            for (int i = 0; i < n; i++)
+            {
+                count = 0;
+                test = cases[i];
+                for (int j = 0; j < test.Length; j++)
+                {
+                    if ((test[j] == 'a') || (test[j] == 'e') || (test[j] == 'o') || (test[j] == 'i') || (test[j] == 'y') || (test[j] == 'u'))
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine($"Number of vowel in text: {count}");
+            }
+            /*string test = Console.ReadLine();
+            for (int i = 0; i < test.Length; i++)
+            {
+                if ((test[i] == 'a') || (test[i] == 'e') || (test[i] == 'o') || (test[i] == 'i') || (test[i] == 'y') || (test[i] == 'u'))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);*/
+            Console.ReadKey();
         }
     }
 }
